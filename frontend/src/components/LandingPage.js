9889246716727baff62a910ffcce5ccbefb0b3cd
Kevin <kevin.gelineau90@gmail.com>
2026-03-09 14:50:53 +0100

    Taille illu

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -309,7 +309,7 @@ const LandingPage = () => {
                 className='bg-white rounded-2xl p-8 text-center shadow-lg'
                 style={{ animationDelay: `${index * 0.2}s` }}
               >
-                <div className='mx-auto mb-6 bg-elysion-primary/10 rounded-2xl flex items-center justify-center'>
+                <div className='w-16 h-16 mx-auto mb-6 bg-elysion-primary/10 rounded-2xl flex items-center justify-center'>
                   <img src={user.icon} alt={user.title} aria-hidden="true" />
                 </div>
                 <h3 className='text-xl font-bold text-elysion-primary mb-4 font-montserrat'>
